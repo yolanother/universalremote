d754556a09b12ae1f0ac4ddee1cac3443d2749f4
Aaron Jackson <yolanother@gmail.com>
2014-02-03 23:46:38 -0600

    Fix paging in slide on remotes

--- src/com/doubtech/universalremote/SlideOnRemote.java
+++ src/com/doubtech/universalremote/SlideOnRemote.java
@@ -6,26 +6,26 @@ import java.util.List;
 import wei.mark.standout.SlideOnWindow;
 import android.graphics.Color;
 import android.net.Uri;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.FileProvider;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.doubtech.universalremote.adapters.ListPageAdapter;
 import com.doubtech.universalremote.io.RemoteConfigurationReader;
 import com.doubtech.universalremote.io.RemoteConfigurationReader.RemotesLoadedListener;
 import com.doubtech.universalremote.utils.Constants;
 import com.doubtech.universalremote.widget.RemotePage;
 
 public class SlideOnRemote extends SlideOnWindow {
-    SectionsPagerAdapter mSectionsPagerAdapter;
     ViewPager mViewPager;
     private Uri mFile;
+	private ListPageAdapter<RemotePage> mPageAdapter;
+	
 
     public SlideOnRemote() {
         super(SlideOnRemote.class);
@@ -44,8 +44,48 @@ public class SlideOnRemote extends SlideOnWindow {
         View v = inflater.inflate(R.layout.activity_remotes, root, true);
         v.setBackgroundColor(Color.argb(180, 0, 0, 0));
         mViewPager = (ViewPager) v.findViewById(R.id.pager);
+        mPageAdapter = new ListPageAdapter<RemotePage>(this) {
+
+    		@Override
+    		public void initView(View v, RemotePage item, int position) {
+    			FrameLayout parent = ((FrameLayout)v);
+    			ViewGroup oldParent = (ViewGroup) item.getParent();
+    			if(null != oldParent) {
+    				oldParent.removeView(item);
+    			}
+    			parent.addView(item);
+    		}
+    		
+    		@Override
+    		public CharSequence getPageTitle(int position) {
+    			return get(position).getTitle();
+    		}
+    	};
         open(mFile);
         return v;
+    }
+
+    public void open(Uri file) {
+        mFile = file;
+        mPageAdapter.clear();
+        mPageAdapter.notifyDataSetChanged();
+        RemoteConfigurationReader reader = new RemoteConfigurationReader(SlideOnRemote.this);
+        reader.open(file, new RemotesLoadedListener() {
+
+            @Override
+            public void onRemotesLoaded(Uri uri, List<RemotePage> pages) {
+                for (RemotePage page : pages) {
+                    mPageAdapter.add(page);
+                    mPageAdapter.notifyDataSetChanged();
+                }
+                mViewPager.setAdapter(mPageAdapter);
+            }
+
+            @Override
+            public void onRemoteLoadFailed(Throwable error) {
+                Toast.makeText(SlideOnRemote.this, error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -55,8 +95,8 @@ public class SlideOnRemote extends SlideOnWindow {
 
     @Override
     public String getPersistentNotificationMessage(int id) {
-        if (null != mSectionsPagerAdapter && mSectionsPagerAdapter.getCount() > 0) {
-            return "" + mSectionsPagerAdapter.mPages.get(mViewPager.getCurrentItem()).getTitle();
+        if (null != mPageAdapter && mPageAdapter.getCount() > 0) {
+            return "" + mPageAdapter.get(mViewPager.getCurrentItem()).getTitle();
         }
         return super.getPersistentNotificationMessage(id);
     }
@@ -69,88 +109,5 @@ public class SlideOnRemote extends SlideOnWindow {
     @Override
     public int getAppIcon() {
         return R.drawable.ic_launcher;
-    }
-
-    private void open(Uri uri) {
-        mSectionsPagerAdapter = new SectionsPagerAdapter();
-        mSectionsPagerAdapter.open(uri);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-    }
-
-    class test extends PagerAdapter {
-
-        @Override
-        public int getCount() {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-
-        @Override
-        public boolean isViewFromObject(View arg0, Object arg1) {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends PagerAdapter {
-        List<RemotePage> mPages = new ArrayList<RemotePage>();
-
-        public SectionsPagerAdapter() {
-        }
-
-        public void open(Uri file) {
-            mFile = file;
-            mPages.clear();
-            notifyDataSetChanged();
-            RemoteConfigurationReader reader = new RemoteConfigurationReader(SlideOnRemote.this);
-            reader.open(file, new RemotesLoadedListener() {
-
-                @Override
-                public void onRemotesLoaded(Uri uri, List<RemotePage> pages) {
-                    for (RemotePage page : pages) {
-                        mPages.add(page);
-                        notifyDataSetChanged();
-                    }
-                }
-
-                @Override
-                public void onRemoteLoadFailed(Throwable error) {
-                    Toast.makeText(SlideOnRemote.this, error.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-
-        @Override
-        public int getCount() {
-            return mPages.size();
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mPages.get(position).getTitle();
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            Log.d("AARON", "Button Count: " + mPages.get(position).getChildCount());
-            container.addView(mPages.get(position));
-            return mPages.get(position);
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView(mPages.get(position));
-            super.destroyItem(container, position, object);
-        }
     }
 }
---
+++ src/com/doubtech/universalremote/adapters/ListPageAdapter.java
@@ -0,0 +1,99 @@
+package com.doubtech.universalremote.adapters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+/**
+ * A page adapter which works with a large data set by reusing views.
+ */
+public abstract class ListPageAdapter<T> extends PagerAdapter {
+
+	// Views that can be reused.
+	private final List<View> mDiscardedViews = new ArrayList<View>();
+	// Views that are already in use.
+	private final SparseArray<View> mBindedViews = new SparseArray<View>();
+
+	private final ArrayList<T> mItems;
+	private final LayoutInflater mInflator;
+	private final int mResourceId;
+	private Context mContext;
+	
+	public ListPageAdapter(Context context) {
+		this(context, 0);
+	}
+
+	public ListPageAdapter(Context context, int viewRes) {
+		mItems = new ArrayList<T>();
+		mInflator = LayoutInflater.from(context);
+		mResourceId = viewRes;
+		mContext = context;
+	}
+
+	@Override
+	public int getCount() {
+		return mItems.size();
+	}
+
+	@Override
+	public boolean isViewFromObject(View v, Object obj) {
+		return v == mBindedViews.get(mItems.indexOf(obj));
+	}
+
+	@Override
+	public void destroyItem(ViewGroup container, int position, Object object) {
+		View view = mBindedViews.get(position);
+		if (view != null) {
+			mDiscardedViews.add(view);
+			mBindedViews.remove(position);
+			container.removeView(view);
+		}
+	}
+
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		View child = null;
+		if(mResourceId != 0) {
+			child = mDiscardedViews.isEmpty() ? mInflator.inflate(mResourceId,
+				container, false) : mDiscardedViews.remove(0);
+		} else {
+			child = mDiscardedViews.isEmpty() ?
+					new FrameLayout(mContext) : mDiscardedViews.remove(0);
+		}
+
+		T data = mItems.get(position);
+		initView(child, data, position);
+
+		mBindedViews.append(position, child);
+		container.addView(child, 0);
+		return data;
+	}
+
+	public void add(T item) {
+		mItems.add(item);
+	}
+
+	public T remove(int position) {
+		return mItems.remove(position);
+	}
+	
+	public T get(int position) {
+		return mItems.get(position);
+	}
+
+	public void clear() {
+		mItems.clear();
+	}
+
+	/**
+	 * Initiate the view here
+	 */
+	public abstract void initView(View v, T item, int position);
+}
